f8b60afeb1a2f4368aed6a182df0953bed8c86a8
Prathamesh Bhalwane <77291248+Prathamesh1222@users.noreply.github.com>
2022-12-22 13:55:08 +0530

    Update FullstackApplication.java

--- src/main/java/com/backend/fullstack/FullstackApplication.java
+++ src/main/java/com/backend/fullstack/FullstackApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FullstackApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
